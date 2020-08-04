70402081f722699047ceda29ce53de30b244c6dc
zgh_ubuntu2020 <zgh_email@163.com>
2020-08-04 13:54:57 +0800

    update the header file

--- include/rrt_bias/rrt_bias.h
+++ include/rrt_bias/rrt_bias.h
@@ -7,6 +7,11 @@
 #include "ompl/tools/config/SelfConfig.h"
 #include "ompl/datastructures/NearestNeighbors.h"
 #include "ompl/geometric/planners/PlannerIncludes.h"
+#include <ompl/base/PlannerStatus.h>
+#include <ompl/base/PlannerTerminationCondition.h>
+#include <ompl/base/State.h>
+#include <ompl/base/StateSpaceTypes.h>
+#include <ompl/base/StateValidityChecker.h>
 
 namespace ompl
 {
@@ -16,18 +21,63 @@ namespace ompl
         class RRT_Bias : public base::Planner
         {
             public:
-                /* @brief Construct */
+                /* 
+                 * @brief: Construct
+                 */
                 RRT_Bias(const base::SpaceInformationPtr &si);
 
                 ~RRT_Bias() override;
                 
-                /* setup  */
-                void setup(); override;
+                /*
+                 * @brief: The only once setup
+                 * @param: None
+                 * @return None
+                 */
+                void setup() override;
+
+                /* @brief: Free the 
+                 * @param: None
+                 * @return None
+                 */
+                void clear() override;
+
+                /*
+                 * @brief: The solution function
+                 */
+                base::PlannerStatus solve(const base::PlannerTerminationCondition &ptc) override;
+
+
             protected:
-                /* */
+                /*
+                 * @brief: Representation of a motion
+                 * @brief: This only contains pointers to parent motions as we
+                 * only need to go backwards in the tree
+                 */
                 class Motion
                 {
-                
+                    public:
+                        Motion() = default;
+
+                        /*
+                         * @brief Constructor that allocates memory for the
+                         * state
+                         */
+                        Motion(const base::SpaceInformationPtr &si) 
+                            : state(si->allocState())
+                        {
+                        }
+
+                        ~Motion() = default;
+
+                        /*
+                         * @brief The state contained by the motion
+                         */
+                        base::State *state{nullptr};
+
+                        /*
+                         * @brief: The parent motion in the exploration tree
+                         */
+                        Motion *parent{nullptr};
                 };
         };
     }
--- src/rrt_bias.cpp
+++ src/rrt_bias.cpp
@@ -1,3 +1,12 @@
-#include "ompl/geometric/planners/cforest/CForest.h"
+#include "../include/rrt_bias/rrt_bias.h"
+#include <ompl/base/StateValidityChecker.h>
 
+ompl::geometric::RRT_Bias::RRT_Bias(const base::SpaceInformationPtr &si)
+    : base::Planner(si, "RRT Bias")
+{
+    specs_.approximateSolutions = true;
+    specs_.directed = true;
 
+}
+
+ompl::geometric::RRT_Bias::~RRT_Bias()
