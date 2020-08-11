e070b281affccdd435436d8c4ed5a763891222f3
zgh_ubuntu2020 <zgh_email@163.com>
2020-08-11 15:19:30 +0800

    add start and goal pose

---
+++ include/planner.h
@@ -0,0 +1,158 @@
+#ifndef _PLANNER_H_
+#define _PLANNER_H_
+
+/*
+ * @brief The ROS related header file
+ */
+#include <ros/publisher.h>
+#include <ros/ros.h>
+#include <ros/node_handle.h>
+#include <ros/subscriber.h>
+// the msg of start and goal pose
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <geometry_msgs/PoseStamped.h>
+// the marker in rviz
+#include <visualization_msgs/Marker.h>
+// the pose array
+#include <geometry_msgs/PoseArray.h>
+// the path in rviz
+#include <nav_msgs/Path.h>
+
+/*
+ * @brief OMPL lib
+ */
+#include <ompl/base/SpaceInformation.h>
+#include <ompl/base/StateValidityChecker.h>
+#include <ompl/geometric/SimpleSetup.h>
+
+/*
+ * @brief The State space
+ */
+#include <ompl/base/spaces/RealVectorStateSpace.h>
+#include <ompl/base/spaces/ReedsSheppStateSpace.h>
+
+
+/*
+ * @brief the namespace
+ */
+namespace ob = ompl::base;
+namespace og = ompl::geometric;
+
+/*
+ * @brief the micro define
+ */
+#define BOUNDARY    (10)
+
+typedef struct _position
+{
+    double x;
+    double y;
+    double yaw;
+}Position;
+
+namespace RRT_planner
+{
+    class Planner
+    {
+        public:
+        /*
+         * @brief Construct function
+         */
+        Planner(void);
+        ~Planner(void);
+
+        /*
+         * @brief Init the planner,such as,the space information, the obstacle
+         * checker and the path cost optimization
+         */
+        void Init(void);
+
+        /*
+         * @brief The solve of planner
+         */
+        void solve(const double time);
+
+        /*
+         * @brief The callback function of start pose
+         */
+        void StartPoseCallback(const geometry_msgs::PoseWithCovarianceStamped &pose);
+
+        /*
+         * @brief The callback function of goal pose
+         */
+        void GoalPoseCallback(const geometry_msgs::PoseStamped &pose);
+        private:
+        /*
+         * @brief the node handle  
+         */
+        ros::NodeHandle n;
+
+        /*
+         * @brief the subscriber for receive the start update
+         */
+        ros::Subscriber start_pose_sub;
+        /*
+         * @brief the subscriber for receive the goal update
+         */
+        ros::Subscriber goal_pose_sub;
+
+        /*
+         * @brief the line marker 
+         */
+        ros::Publisher line_marker_pub;
+
+        /*
+         * @brief the pose array publisher
+         */
+        ros::Publisher pose_array_pub;
+
+        /*
+         * @brief the start position line strip
+         */
+        visualization_msgs::Marker _start_pose_line_strip;
+
+        /*
+         * @brief the goal position line strip
+         */
+        visualization_msgs::Marker _goal_pose_line_strip;
+
+        /*
+         * @brief the rrt sample point show in rviz
+         */
+        geometry_msgs::PoseArray _sample_point; 
+
+        /*
+         * @brief the plan path by the planner
+         */
+        nav_msgs::Path _plan_path;
+
+        /*
+         * @brief The Space information
+         */
+        ob::SpaceInformationPtr _si;
+
+        /*
+         * @brief The state space
+         */
+        ob::StateSpacePtr _state_space;
+        /*
+         * @brief The simple setup pointer
+         */
+        og::SimpleSetupPtr _ss;
+
+        /*
+         * @brief start position
+         */
+        Position _start_position;
+        bool     _start_valid;
+
+        /*
+         * @brief goal position
+         */
+        Position _goal_position;
+        bool     _goal_valid;
+
+    };
+}
+
+#endif
---
+++ src/planner.cpp
@@ -0,0 +1,202 @@
+/*
+ * @brief The planner 
+ */
+#include "../include/planner.h"
+#include "../include/rrt_bias.h"
+#include "../include/obstacle_checker.h"
+
+// the tf
+#include <geometry_msgs/PoseArray.h>
+#include <ompl/base/ScopedState.h>
+#include <ros/time.h>
+#include <tf/transform_datatypes.h>
+
+/*
+ * @brief OMPL lib
+ */
+#include <ompl/base/PlannerStatus.h>
+#include <ompl/base/SpaceInformation.h>
+#include <ompl/base/StateSampler.h>
+#include <ompl/base/spaces/RealVectorBounds.h>
+#include <ompl/base/spaces/SE2StateSpace.h>
+
+/*
+ * @brief System lib
+ */
+#include <valarray>
+#include <memory>
+#include <visualization_msgs/Marker.h>
+
+
+RRT_planner::Planner::Planner(void)
+{
+    _start_valid = false;
+    _goal_valid  = false;
+
+    // the line strip init
+    _start_pose_line_strip.header.frame_id = 
+    _goal_pose_line_strip.header.frame_id  =
+    _sample_point.header.frame_id =
+    "map";
+
+
+    _start_pose_line_strip.header.stamp = 
+    _goal_pose_line_strip.header.stamp  =
+    _sample_point.header.stamp = 
+    ros::Time::now();
+
+     _start_pose_line_strip.ns = "start_pose_line_strip";
+     _goal_pose_line_strip.ns  = "goal_pose_line_strip";
+
+     _start_pose_line_strip.action = 
+     _goal_pose_line_strip.action  =
+     visualization_msgs::Marker::MODIFY;
+
+     _start_pose_line_strip.id = 0;
+     _goal_pose_line_strip.id  = 1;
+
+
+     _start_pose_line_strip.type = 
+     _goal_pose_line_strip.type  =
+     visualization_msgs::Marker::ARROW;
+
+    // the line strip scale 
+    _start_pose_line_strip.scale.x = _goal_pose_line_strip.scale.x = 1;
+    _start_pose_line_strip.scale.y = _goal_pose_line_strip.scale.y = 0.1;
+    _start_pose_line_strip.scale.z = _goal_pose_line_strip.scale.z = 0.1;
+
+    _start_pose_line_strip.color.b = 1.0;
+    _start_pose_line_strip.color.a = 1.0;
+
+    _goal_pose_line_strip.color.g = 1.0;
+    _goal_pose_line_strip.color.a = 1.0;
+
+    line_marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 100);
+    pose_array_pub = n.advertise<geometry_msgs::PoseArray>("pose_array", 100);
+    // TODO 
+    start_pose_sub = n.subscribe("initialpose", 1, &RRT_planner::Planner::StartPoseCallback, this);
+    goal_pose_sub  = n.subscribe("move_base_simple/goal", 1, &RRT_planner::Planner::GoalPoseCallback, this);
+}
+
+RRT_planner::Planner::~Planner(void)
+{
+    // TODO free memory
+}
+
+/*
+ * @brief The initial function
+ */
+void RRT_planner::Planner::Init(void)
+{
+    ob::RealVectorBounds obstacle_boundary = ob::RealVectorBounds(2);
+
+     
+    obstacle_boundary.setLow(0, -BOUNDARY);
+    obstacle_boundary.setLow(1, -BOUNDARY);
+
+    obstacle_boundary.setHigh(0, BOUNDARY);
+    obstacle_boundary.setHigh(1, BOUNDARY);
+
+    _state_space = std::make_shared<ob::ReedsSheppStateSpace>(5.0);
+    _state_space->as<ob::SE2StateSpace>()->setBounds(obstacle_boundary);
+
+    _si = std::make_shared<ob::SpaceInformation>(_state_space);
+    _si->setStateValidityChecker(std::make_shared<RRT_planner::ObstacleChecker>(_si));
+    _si->setStateValidityCheckingResolution(0.03);
+    _si->setup();
+
+    _ss = std::make_shared<og::SimpleSetup>(_si);
+    _ss->setPlanner(std::make_shared<og::RRT_Bias>(_si));
+}
+
+/*
+ * @brief The solve function
+ */
+void RRT_planner::Planner::solve(const double time)
+{
+    if (_start_valid && _goal_valid)
+    {
+        ob::ScopedState<> start_state(_state_space);
+        ob::ScopedState<> goal_state(_state_space);
+
+        start_state->as<ob::SE2StateSpace::StateType>()->setX(_start_position.x);
+        start_state->as<ob::SE2StateSpace::StateType>()->setY(_start_position.y);
+        start_state->as<ob::SE2StateSpace::StateType>()->setYaw(_start_position.yaw);
+
+        goal_state->as<ob::SE2StateSpace::StateType>()->setX(_goal_position.x);
+        goal_state->as<ob::SE2StateSpace::StateType>()->setY(_goal_position.y);
+        goal_state->as<ob::SE2StateSpace::StateType>()->setYaw(_goal_position.yaw);
+
+        _ss->setStartAndGoalStates(start_state, goal_state);
+
+        _ss->setup();
+        _ss->print();
+
+        ob::PlannerStatus solved = _ss->solve(time);
+
+        if (solved)
+        {
+            _sample_point.poses.clear();
+            geometry_msgs::Pose pose_temp;
+            for (auto &state : _ss->getSolutionPath().getStates())
+            {
+                const auto *SE2_state = state->as<ob::SE2StateSpace::StateType>();
+                pose_temp.position.x = SE2_state->getX();
+                pose_temp.position.y = SE2_state->getY();
+                tf::quaternionTFToMsg(tf::createQuaternionFromYaw(SE2_state->getYaw()), 
+                                                                pose_temp.orientation);
+
+                _sample_point.poses.push_back(pose_temp);
+            }
+            pose_array_pub.publish(_sample_point);
+        }
+        else 
+        {
+            
+        }
+        _ss->clear();
+        _goal_valid = false;
+    }
+}
+
+/*
+ * @brief The callback function of start position
+ */
+void RRT_planner::Planner::StartPoseCallback(const geometry_msgs::PoseWithCovarianceStamped &pose)
+{
+    _start_position.x = pose.pose.pose.position.x;
+    _start_position.y = pose.pose.pose.position.y;
+    _start_position.yaw = tf::getYaw(pose.pose.pose.orientation);
+
+    _start_valid = true;
+
+    _start_pose_line_strip.pose = pose.pose.pose;
+
+    line_marker_pub.publish(_start_pose_line_strip);
+
+    ROS_INFO("start position x:%f, y:%f, yaw:%f",   _start_position.x,
+                                                    _start_position.y,
+                                                    _start_position.yaw);
+
+}
+
+/*
+ * @brief The callback function of goal position
+ */
+void RRT_planner::Planner::GoalPoseCallback(const geometry_msgs::PoseStamped &pose)
+{
+    _goal_position.x = pose.pose.position.x;
+    _goal_position.y = pose.pose.position.y;
+    _goal_position.yaw = tf::getYaw(pose.pose.orientation);
+
+    _goal_valid = true;
+
+    _goal_pose_line_strip.pose = pose.pose;
+
+    line_marker_pub.publish(_goal_pose_line_strip);
+
+    ROS_INFO("goal position x:%f, y:%f, yaw:%f", _goal_position.x,
+                                                 _goal_position.y,
+                                                 _goal_position.yaw);
+}
+
